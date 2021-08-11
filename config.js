c25fa791cf8bba86a1336d359b5c054a896d81c7
DuongSon <88789749+dssaker@users.noreply.github.com>
2021-08-11 23:16:43 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hi cậu iuuu!',
+    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'hihi',
+    title: 'Cậu yêu Tớ có phải không nào 😙',
+    desc: 'nếu Cậu không trả lời mà thoát ra tức là cậu yêu tớ rùi đó nha :v ',
+    btnYes: 'Yêu ơi là yêu <33',
+    btnNo: 'Cậu mơ à :3',
+    question:' Nói cho tớ lý do cậu thích tớ đi =))))',
+    btnReply: 'Gửi cho Tớ <3',
+    reply: 'Vì cậu đẹp trai đó',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Tối nay tớ qua đón cậu đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooooooa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
